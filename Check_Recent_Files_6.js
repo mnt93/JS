5cffbd9530b08174d45eea1543c9a168d4ba9381
mnt93 <81918188+mnt93@users.noreply.github.com>
2023-02-02 16:31:46 +0100

    Update and rename Check_Recent_Files_5.js to Check_Recent_Files_6.js

--- Check_Recent_Files_6.js
+++ Check_Recent_Files_6.js
@@ -126,7 +126,6 @@ function load_url_array(url) {
   if (xmlhttp.readyState == 4 & xmlhttp.status == 200) {
   	url_array = xmlhttp.responseText.split('\n');
   }
-  url_array.pop();
     return url_array
 }
 
